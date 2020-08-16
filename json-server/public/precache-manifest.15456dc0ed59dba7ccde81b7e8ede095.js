ccdea3f40316df4955442763b5a620efde27493b
Imane El Harti <elharti.imane.98@gmail.com>
2020-08-17 00:04:50 +0100

    first commit

---
+++ json-server/public/precache-manifest.15456dc0ed59dba7ccde81b7e8ede095.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94164d38ab56c41dba159b8949511cba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "864cd9faabe68d69efcc",
+    "url": "/static/css/2.1bd367ea.chunk.css"
+  },
+  {
+    "revision": "a8ddbc93106ae28c4f3d",
+    "url": "/static/css/main.ff87bdd6.chunk.css"
+  },
+  {
+    "revision": "864cd9faabe68d69efcc",
+    "url": "/static/js/2.77ab75cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.77ab75cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ddbc93106ae28c4f3d",
+    "url": "/static/js/main.c6a209e7.chunk.js"
+  },
+  {
+    "revision": "79298229a104881fd483",
+    "url": "/static/js/runtime-main.ad013ee6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
